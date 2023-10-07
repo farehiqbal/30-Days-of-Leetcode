cebb53c05ced4a986e00775f03955e955c0bb3ea
Fareh Iqbal <fareh.legend@gmail.com>
2023-10-08 03:51:51 +0500

    Time: 1 ms (41.39%), Space: 40.4 MB (17.19%) - LeetHub

--- 1584-average-salary-excluding-the-minimum-and-maximum-salary/1584-average-salary-excluding-the-minimum-and-maximum-salary.java
+++ 1584-average-salary-excluding-the-minimum-and-maximum-salary/1584-average-salary-excluding-the-minimum-and-maximum-salary.java
@@ -1,17 +1,21 @@
 class Solution {
     public double average(int[] salary) {
-            int min = Arrays.stream(salary).min().getAsInt();
-            int max = Arrays.stream(salary).max().getAsInt();
+            // int min = Arrays.stream(salary).min().getAsInt();
+            // int max = Arrays.stream(salary).max().getAsInt();
+
+            Arrays.sort(salary);
+
 
             double average = 0;
 
-            for(int i =0 ; i < salary.length; i++){
+            for(int i =1 ; i < salary.length -1; i++){
 
-                if(salary[i] == max || salary[i] == min) continue;
+                // if(salary[i] == max || salary[i] == min) continue;
 
-                else{
-                    average += salary[i];
-                }
+                // else{
+                //     average += salary[i];
+                // }
+                average += salary[i];
             }
 
             return average / (salary.length - 2);
