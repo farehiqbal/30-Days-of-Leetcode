0951367f82dbee24e8dcba078b7d437ee328d901
Fareh Iqbal <fareh.legend@gmail.com>
2023-10-08 04:14:26 +0500

    Time: 0 ms (100%), Space: 43 MB (45.2%) - LeetHub

---
+++ 1349-check-if-it-is-a-straight-line/1349-check-if-it-is-a-straight-line.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean checkStraightLine(int[][] coordinates) {
+    if (coordinates.length <= 2) {
+        return true; // Any two points are collinear
+    }
+
+    int dx = coordinates[1][0] - coordinates[0][0];
+    int dy = coordinates[1][1] - coordinates[0][1];
+
+    for (int i = 2; i < coordinates.length; i++) {
+        int dx2 = coordinates[i][0] - coordinates[i - 1][0];
+        int dy2 = coordinates[i][1] - coordinates[i - 1][1];
+
+        if (dx2 * dy != dx * dy2) {
+            return false; // Points are not collinear
+        }
+    }
+
+    return true; // All points are collinear
+}
+
+
+    // public int difference(int x1, int x2, int y1, int y2){
+
+    //     return Math.sqrt (((x1 - x2) * 2) - ((y1 - y2) * 2));
+    // }
+}
