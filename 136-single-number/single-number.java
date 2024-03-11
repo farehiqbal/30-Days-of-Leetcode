6df83b052b73d0813279496f380ce034cfa17e55
Fareh Iqbal <fareh.legend@gmail.com>
2024-03-12 00:55:48 +0500

    Time: 1 ms (99.85%) | Memory: 46.6 MB (6.36%) - LeetSync

---
+++ 136-single-number/single-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        
+        if(nums.length == 1) return nums[0];
+
+        int result =0;
+        for(int i = 0; i< nums.length; i++){
+
+            result = result ^ nums[i];
+        }
+
+
+
+        return result;
+    }
+}
