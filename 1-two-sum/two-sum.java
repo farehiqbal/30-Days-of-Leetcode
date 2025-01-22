8a9c6e8d17dea03e2a6e18189fbb5f6088241996
Fareh Iqbal <fareh.legend@gmail.com>
2025-01-23 01:44:08 +0500

    Time: 4 ms (60.78%) | Memory: 44.7 MB (93.18%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,14 +1,37 @@
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
+
+    // brute force approach
+    // public int[] twoSum(int[] nums, int target) {
         
-        Map<Integer, Integer> numToIndex = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (numToIndex.containsKey(target - nums[i])) {
-                return new int[] {numToIndex.get(target - nums[i]), i};
-            }
-            numToIndex.put(nums[i], i);
+    //     int i, j;
+    //     for (i = 0; i < nums.length; i++){
+    //         for (j = i+1; j < nums.length; j++){
+    //             if (nums[i] + nums[j] == target){
+    //                 return new int[]{i, j}; 
+    //             }
+    //         }
+    //     }
+
+    //     return new int[]{-1, -1};
+    // }
+
+        public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+        int n = nums.length;
+
+        // Build the hash table
+        for (int i = 0; i < n; i++) {
+            numMap.put(nums[i], i);
         }
-        return new int[] {};
-        
+
+        // Find the complement
+        for (int i = 0; i < n; i++) {
+            int complement = target - nums[i];
+            if (numMap.containsKey(complement) && numMap.get(complement) != i) {
+                return new int[]{i, numMap.get(complement)};
+            }
+        }
+
+        return new int[]{}; // No solution found
     }
 }
